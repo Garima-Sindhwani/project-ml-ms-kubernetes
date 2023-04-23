18ec926a4eaddba1e8937fe96d4e689532da1def
gsindhwani <34907906+Garima-Sindhwani@users.noreply.github.com>
2023-04-23 09:46:52 +0530

    done till task-6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="gsindhwani/ml-microservice"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy ml-microservice --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+pod=kubectl get pods -o name
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $pod --address 0.0.0.0 8000:80
